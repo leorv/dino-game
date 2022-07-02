6ce0de6bd4a78d8813355745633bdde9ce88bb3e
Leonardo Ruoso Vendramini <leonardo_vendramini@hotmail.com>
2022-07-01 22:18:18 -0300

    complete game

--- script.js
+++ script.js
@@ -1,23 +1,31 @@
 const dino = document.querySelector('.dino');
+const background = document.querySelector('.background');
+
+let isJumping = false;
+let position = 0;
 
 console.log(dino);
 
 function handleKeyDown(event) {
     if (event.keyCode == 32) {
-        jump();
+        if (!isJumping){
+            jump();
+        }        
     }
 }
 
 function jump() {
-    let position = 0;
+
+    isJumping = true;
 
     let upInterval = setInterval(() => {
-        if (position >= 150) {
+        if (position >= 200) {
             clearInterval(upInterval)
             // descendo
             let downInterval = setInterval(() => {
                 if (position <= 0) {
                     clearInterval(downInterval);
+                    isJumping = false;
                 } else {
                     position -= 20;
                     dino.style.bottom = position + 'px';
@@ -30,5 +38,34 @@ function jump() {
         }
     }, 20);
 }
+
+function createCactus() {
+    const cactus = document.createElement('div');
+    let cactusPosition = 1000;
+    let randomTime = Math.random()*6000;
+
+    cactus.classList.add('cactus');
+    cactus.style.left = 1000 + 'px'; 
+    background.appendChild(cactus);
+
+    let leftInterval = setInterval(() => {
+        if (cactusPosition < -60) {
+            clearInterval(leftInterval);
+            background.removeChild(cactus);
+        } else if (cactusPosition > 0 && cactusPosition < 60 && position < 60){
+            // colisão, game over.
+            clearInterval(leftInterval);
+            document.body.innerHTML = '<h1 class="game-over">Game over</h1>';
+
+        } else {
+            cactusPosition -= 10;
+            cactus.style.left = cactusPosition + 'px';
+        }
+    }, 20);
+
+    setTimeout(createCactus, randomTime);
+}
+
+createCactus();
 
 document.addEventListener('keydown', handleKeyDown)
